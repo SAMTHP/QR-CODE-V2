8dd173a8eed1588bfbb54639e55d45d17e82fcc0
Samir Founou <samir.founou@avizzeo.com>
2020-02-03 19:08:32 +0100

    MAJ

--- src/Migrations/Version20200203125200.php
+++ src/Migrations/Version20200203125200.php
@@ -22,10 +22,17 @@ final class Version20200203125200 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        // $this->addSql('CREATE TABLE discount_tag (discount_id INT NOT NULL, tag_id INT NOT NULL, INDEX IDX_E519AE844C7C611F (discount_id), INDEX IDX_E519AE84BAD26311 (tag_id), PRIMARY KEY(discount_id, tag_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE discount_tag ADD CONSTRAINT FK_E519AE844C7C611F FOREIGN KEY (discount_id) REFERENCES discount (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE discount_tag ADD CONSTRAINT FK_E519AE84BAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE');
-        $this->addSql('DROP TABLE tag_discount');
+        $this->addSql('CREATE TABLE discount (id INT AUTO_INCREMENT NOT NULL, company_id INT NOT NULL, start_date DATE NOT NULL, end_date DATE NOT NULL, max_flash INT DEFAULT NULL, link VARCHAR(255) NOT NULL, INDEX IDX_E1E0B40E979B1AD6 (company_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE discount_user (discount_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_1311D3F34C7C611F (discount_id), INDEX IDX_1311D3F3A76ED395 (user_id), PRIMARY KEY(discount_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tag (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tag_discount (tag_id INT NOT NULL, discount_id INT NOT NULL, INDEX IDX_D7D1CFD5BAD26311 (tag_id), INDEX IDX_D7D1CFD54C7C611F (discount_id), PRIMARY KEY(tag_id, discount_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, has_agreed TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE company (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, contact VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE discount ADD CONSTRAINT FK_E1E0B40E979B1AD6 FOREIGN KEY (company_id) REFERENCES company (id)');
+        $this->addSql('ALTER TABLE discount_user ADD CONSTRAINT FK_1311D3F34C7C611F FOREIGN KEY (discount_id) REFERENCES discount (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE discount_user ADD CONSTRAINT FK_1311D3F3A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE tag_discount ADD CONSTRAINT FK_D7D1CFD5BAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE tag_discount ADD CONSTRAINT FK_D7D1CFD54C7C611F FOREIGN KEY (discount_id) REFERENCES discount (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE discount CHANGE max_flash max_flash INT DEFAULT NULL');
     }
 
