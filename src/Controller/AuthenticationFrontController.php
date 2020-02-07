6ddc0a1b22aecaea94fc6a5e54f23afe30fd2a70
Samir Founou <samir.founou@avizzeo.com>
2020-02-08 00:23:47 +0100

    MAJ

--- src/Controller/AuthenticationFrontController.php
+++ src/Controller/AuthenticationFrontController.php
@@ -8,13 +8,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AuthenticationFrontController extends AbstractController
 {
     /**
-     * @Route("/loginApp", name="authentication_login", methods={"GET"})
+     * @Route("/loginApp", name="authentication_login", methods={"POST"})
      */
-    public function index(UserRepository $userRepository, Request $request)
+    public function loginApp(UserRepository $userRepository, Request $request,UserPasswordEncoderInterface $userPasswordEncoderInterface)
     {
         // Get data with request
         $data = $request->getContent();
@@ -25,9 +26,12 @@ class AuthenticationFrontController extends AbstractController
         // Get User by email from request
         $user = $userRepository->findOneByEmail($decodeData->email);
 
+        // hasValid equal true if password valid or false if is not
+        $hashValid = $userPasswordEncoderInterface->isPasswordValid($user, $decodeData->password);
+        
         // Test if login is valid
         if($user){
-            if($decodeData->password == $user->getPassword()) {
+            if($hashValid) {
                 return new JsonResponse("success", Response::HTTP_OK, [], true);
             } else {
                 return new JsonResponse("failed", Response::HTTP_OK, [], true);
