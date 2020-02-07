567ab5bb70fc72310cd84f16ce5ce884ac6c2874
Samir Founou <samir.founou@avizzeo.com>
2020-02-07 23:29:50 +0100

    MAJ

--- src/Controller/AuthenticationFrontController.php
+++ src/Controller/AuthenticationFrontController.php
@@ -12,7 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AuthenticationFrontController extends AbstractController
 {
     /**
-     * @Route("/loginApp", name="authentication_login", methods={"POST"})
+     * @Route("/loginApp", name="authentication_login", methods={"GET"})
      */
     public function index(UserRepository $userRepository, Request $request)
     {
