1f55a931530807f3a628f4af7bb66cd85c880246
Samir Founou <samir.founou@avizzeo.com>
2020-02-07 22:37:54 +0100

    MAJ

--- src/Controller/AuthenticationFrontController.php
+++ src/Controller/AuthenticationFrontController.php
@@ -2,23 +2,19 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AuthenticationFrontController extends AbstractController
 {
     /**
-     * @Route("/authentication", name="authentication_login", methods={"POST"})
+     * @Route("/login", name="authentication_login", methods={"POST"})
      */
-    public function index(UserRepository $userRepository, ValidatorInterface $validatorInterface, EntityManagerInterface $manager, Request $request,SerializerInterface $serializerInterface)
+    public function index(UserRepository $userRepository, Request $request)
     {
         // Get data with request
         $data = $request->getContent();
