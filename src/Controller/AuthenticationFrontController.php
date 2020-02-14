e262ddc0d0e029e0b983deed95eb2c1bbd5bc4b6
Samir Founou <samir.founou@avizzeo.com>
2020-02-14 13:50:11 +0100

    MAJ

--- src/Controller/AuthenticationFrontController.php
+++ src/Controller/AuthenticationFrontController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
-use App\Services\UserDto;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
