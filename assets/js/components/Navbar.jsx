8b23334a5b07a538fa9215b4afc7d8ba77c21fa9
Samir Founou <samir.founou@avizzeo.com>
2020-02-19 12:49:29 +0100

    MAJ

--- assets/js/components/Navbar.jsx
+++ assets/js/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = (props) => {
     return ( 
         <nav className="navbar navbar-expand-lg navbar-light bg-light shadow">
-            <a className="navbar-brand" href="#">QR-CODE-APP</a>
+            <a className="navbar-brand" href="/">QR-CODE-APP</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
