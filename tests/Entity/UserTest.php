29b79285f845db0080880a638fae26043df41031
Samir Founou <samir.founou@avizzeo.com>
2020-03-07 23:46:19 +0100

    MAJ

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -1,16 +1,55 @@
 <?php
 namespace App\Tests\Entity;
 
+use App\Entity\User;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
+    private $user;
+
+    public function setUp(): void
+    {
+        $this->user = new User();
+    }
+
     /**
      * @test
      */
-    public function test()
+    public function userShouldHaveLastName()
     {
-        $this->assertSame(4,43);
+        $this->user->setLastname("nametest");
+        
+        $this->assertEquals("nametest", $this->user->getLastname());
     }
 
+    /**
+     * @test
+     */
+    public function userShouldHaveFirstName()
+    {
+        $this->user->setFirstName("firsttest");
+        
+        $this->assertEquals("firsttest", $this->user->getFirstName());
+    }
+
+    /**
+     * @test
+     */
+    public function userShouldHaveEmail()
+    {
+        $this->user->setEmail("test@test.fr");
+        
+        $this->assertEquals("test@test.fr", $this->user->getEmail());
+    }
+
+    /**
+     * @test
+     */
+    public function userShouldHavePassword()
+    {
+        $this->user->setPassword("pass");
+        
+        $this->assertEquals("pass", $this->user->getPassword());
+    }
 }
